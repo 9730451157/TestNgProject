e8e9b79ecddae644584ac3ec06cc96b9cbf1dc1f
unknown <pankajtayade369@gmail.com>
2022-11-07 12:47:14 +0530

    first commit

---
+++ src/main/java/com/test/TestNgProgram.java
@@ -0,0 +1,44 @@
+package com.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestNgProgram {
+
+	WebDriver driver;
+
+	@BeforeTest
+
+	public void launchBrowser() {
+
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+
+		driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.get("https://classic.freecrm.com/index.html?e=1");
+
+	}
+
+	@Test
+
+	public void titleTest() {
+
+		String title = driver.getTitle();
+
+		System.out.println(title);
+	}
+
+	@AfterTest
+
+	public void closedBrowser() {
+
+		driver.close();
+
+	}
+
+}
